679096bac350c33e1af8fd1483271aaea11911b6
Ian Jones <jones58ian@gmail.com>
2014-09-29 20:57:07 -0700

    Added classes for Comment Service and Lecture Service. (actually saved my changes this time)

--- LectioServer/LectioService/Interfaces/ICommentService.cs
+++ LectioServer/LectioService/Interfaces/ICommentService.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LectioService.Entities;
 
 namespace LectioService.Interfaces
 {
     interface ICommentService
     {
+
+        List<Comment> GetComments(Video video, int pg, int num);
+
+        void AddNewComment(ApplicationUser user, Comment comment, Video video);
     }
 }
