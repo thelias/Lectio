09ef6af9252e1ce18be8b51618ef53f1e136b7ec
thelias <eliastheviking@gmail.com>
2014-10-13 16:51:42 -0700

    new frontend

---
+++ FrontEnd/app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name helloApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the helloApp
+ */
+angular.module('helloApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+        $scope.openModal = function(){
+                $("#myModal").modal('show');
+        }
+  });
